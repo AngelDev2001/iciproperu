ab07b87b8a8aa829dfcdcf788af1dd27ec6ecc02
Angel Gala <galafloresangelemilio@gmail.com>
2026-03-07 18:53:29 -0500

    removed firebase config

--- packages/contracts/src/shared/types.ts
+++
@@ -1,8 +0,0 @@
-export type ISODateString = string;
-
-export interface DefaultFirestoreProps {
-  createdAt: ISODateString;
-  updatedAt?: ISODateString;
-  deletedAt?: ISODateString;
-  isDeleted: boolean;
-}
