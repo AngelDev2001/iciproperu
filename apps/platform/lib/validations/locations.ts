f67420370065ff324e8f7b3b58c4323a82daba84
Angel Gala <galafloresangelemilio@gmail.com>
2026-03-18 18:22:02 -0500

    feat(platform): add validation schemas for locations and addresses

---
+++ apps/platform/lib/validations/locations.ts
@@ -0,0 +1,403 @@
+export const PERU_LOCATIONS = [
+  {
+    id: '01',
+    name: 'Amazonas',
+    provinces: [
+      {
+        id: '0101',
+        name: 'Chachapoyas',
+        districts: ['Chachapoyas', 'Asunción', 'Balsa', 'Leymebamba', 'Soloco'],
+      },
+      { id: '0102', name: 'Bagua', districts: ['Bagua', 'Aramango', 'Copallin', 'La Peca'] },
+    ],
+  },
+  {
+    id: '02',
+    name: 'Áncash',
+    provinces: [
+      { id: '0201', name: 'Huaraz', districts: ['Huaraz', 'Independencia', 'Jangas', 'Tarica'] },
+      {
+        id: '0218',
+        name: 'Santa',
+        districts: ['Chimbote', 'Nuevo Chimbote', 'Coishco', 'Moro', 'Nepeña'],
+      },
+    ],
+  },
+  {
+    id: '03',
+    name: 'Apurímac',
+    provinces: [
+      { id: '0301', name: 'Abancay', districts: ['Abancay', 'Chacoche', 'Curahuasi', 'Tamburco'] },
+      {
+        id: '0302',
+        name: 'Andahuaylas',
+        districts: ['Andahuaylas', 'San Jerónimo', 'Talavera', 'Pacucha'],
+      },
+    ],
+  },
+  {
+    id: '04',
+    name: 'Arequipa',
+    provinces: [
+      {
+        id: '0401',
+        name: 'Arequipa',
+        districts: [
+          'Arequipa',
+          'Cayma',
+          'Cerro Colorado',
+          'Yanahuara',
+          'Selva Alegre',
+          'Jose Luis Bustamante y Rivero',
+          'Miraflores',
+        ],
+      },
+      { id: '0402', name: 'Camaná', districts: ['Camaná', 'Jose Maria Quimper', 'Samuel Pastor'] },
+      {
+        id: '0407',
+        name: 'Islay',
+        districts: ['Mollendo', 'Cocachacra', 'Dean Valdivia', 'Mejia'],
+      },
+    ],
+  },
+  {
+    id: '05',
+    name: 'Ayacucho',
+    provinces: [
+      {
+        id: '0501',
+        name: 'Huamanga',
+        districts: ['Ayacucho', 'Carmen Alto', 'Chiara', 'Jesus Nazareno', 'San Juan Bautista'],
+      },
+      { id: '0504', name: 'Huanta', districts: ['Huanta', 'Ayahuanco', 'Iguain', 'Luricocha'] },
+    ],
+  },
+  {
+    id: '06',
+    name: 'Cajamarca',
+    provinces: [
+      {
+        id: '0601',
+        name: 'Cajamarca',
+        districts: ['Cajamarca', 'Baños del Inca', 'Encañada', 'Namora'],
+      },
+      { id: '0608', name: 'Jaén', districts: ['Jaén', 'Bellavista', 'Pucara', 'San Felipe'] },
+    ],
+  },
+  {
+    id: '07',
+    name: 'Callao',
+    provinces: [
+      {
+        id: '0701',
+        name: 'Callao',
+        districts: [
+          'Callao',
+          'Bellavista',
+          'Carmen de la Legua',
+          'La Perla',
+          'La Punta',
+          'Ventanilla',
+          'Mi Perú',
+        ],
+      },
+    ],
+  },
+  {
+    id: '08',
+    name: 'Cusco',
+    provinces: [
+      {
+        id: '0801',
+        name: 'Cusco',
+        districts: ['Cusco', 'San Jerónimo', 'San Sebastián', 'Santiago', 'Wanchaq', 'Saylla'],
+      },
+      {
+        id: '0813',
+        name: 'Urubamba',
+        districts: ['Urubamba', 'Chinchero', 'Machupicchu', 'Ollantaytambo'],
+      },
+    ],
+  },
+  {
+    id: '09',
+    name: 'Huancavelica',
+    provinces: [
+      {
+        id: '0901',
+        name: 'Huancavelica',
+        districts: ['Huancavelica', 'Ascensión', 'Acobambilla', 'Palca'],
+      },
+    ],
+  },
+  {
+    id: '10',
+    name: 'Huánuco',
+    provinces: [
+      {
+        id: '1001',
+        name: 'Huánuco',
+        districts: ['Huánuco', 'Amarilis', 'Chinchao', 'Pillco Marca'],
+      },
+      {
+        id: '1006',
+        name: 'Leoncio Prado',
+        districts: ['Rupa-Rupa (Tingo María)', 'Castillo Grande', 'Luyando'],
+      },
+    ],
+  },
+  {
+    id: '11',
+    name: 'Ica',
+    provinces: [
+      {
+        id: '1101',
+        name: 'Ica',
+        districts: ['Ica', 'La Tinguiña', 'Parcona', 'Salas', 'Subtanjalla', 'Santiago'],
+      },
+      {
+        id: '1102',
+        name: 'Chincha',
+        districts: ['Chincha Alta', 'Chincha Baja', 'Grocio Prado', 'Pueblo Nuevo', 'Sunampe'],
+      },
+      {
+        id: '1105',
+        name: 'Pisco',
+        districts: ['Pisco', 'Huancano', 'Paracas', 'San Andres', 'San Clemente'],
+      },
+    ],
+  },
+  {
+    id: '12',
+    name: 'Junín',
+    provinces: [
+      {
+        id: '1201',
+        name: 'Huancayo',
+        districts: ['Huancayo', 'Chilca', 'El Tambo', 'Huancan', 'Sapallanga'],
+      },
+      {
+        id: '1203',
+        name: 'Chanchamayo',
+        districts: ['Chanchamayo (La Merced)', 'Pichanaqui', 'San Ramon'],
+      },
+      { id: '1208', name: 'Tarma', districts: ['Tarma', 'Acobamba', 'Palca'] },
+    ],
+  },
+  {
+    id: '13',
+    name: 'La Libertad',
+    provinces: [
+      {
+        id: '1301',
+        name: 'Trujillo',
+        districts: [
+          'Trujillo',
+          'El Porvenir',
+          'Florencia de Mora',
+          'Huanchaco',
+          'La Esperanza',
+          'Laredo',
+          'Moche',
+          'Victor Larco Herrera',
+        ],
+      },
+      { id: '1302', name: 'Ascope', districts: ['Ascope', 'Casa Grande', 'Chicama', 'Paijan'] },
+      { id: '1308', name: 'Sánchez Carrión', districts: ['Huamachuco', 'Chugay', 'Sarin'] },
+    ],
+  },
+  {
+    id: '14',
+    name: 'Lambayeque',
+    provinces: [
+      {
+        id: '1401',
+        name: 'Chiclayo',
+        districts: [
+          'Chiclayo',
+          'Chongoyape',
+          'Jose Leonardo Ortiz',
+          'La Victoria',
+          'Pimentel',
+          'Reque',
+          'Santa Rosa',
+        ],
+      },
+      {
+        id: '1403',
+        name: 'Lambayeque',
+        districts: ['Lambayeque', 'Jayanca', 'Motupe', 'Olmos', 'Tucume'],
+      },
+    ],
+  },
+  {
+    id: '15',
+    name: 'Lima',
+    provinces: [
+      {
+        id: '1501',
+        name: 'Lima',
+        districts: [
+          'Cercado de Lima',
+          'Ate',
+          'Barranco',
+          'Breña',
+          'Chorrillos',
+          'Comas',
+          'El Agustino',
+          'Jesus Maria',
+          'La Molina',
+          'La Victoria',
+          'Lince',
+          'Los Olivos',
+          'Magdalena del Mar',
+          'Miraflores',
+          'Pueblo Libre',
+          'Puente Piedra',
+          'Rimac',
+          'San Borja',
+          'San Isidro',
+          'San Juan de Lurigancho',
+          'San Juan de Miraflores',
+          'San Luis',
+          'San Martin de Porres',
+          'San Miguel',
+          'Santa Anita',
+          'Santiago de Surco',
+          'Surquillo',
+          'Villa El Salvador',
+          'Villa Maria del Triunfo',
+        ],
+      },
+      {
+        id: '1505',
+        name: 'Cañete',
+        districts: ['San Vicente de Cañete', 'Asia', 'Imperial', 'Lunahuana', 'Mala', 'Chilca'],
+      },
+      { id: '1506', name: 'Huaral', districts: ['Huaral', 'Chancay', 'Aucallama'] },
+      {
+        id: '1508',
+        name: 'Huaura',
+        districts: ['Huacho', 'Hualmay', 'Huaura', 'Sayan', 'Vegueta'],
+      },
+    ],
+  },
+  {
+    id: '16',
+    name: 'Loreto',
+    provinces: [
+      {
+        id: '1601',
+        name: 'Maynas',
+        districts: ['Iquitos', 'Alto Nanay', 'Belen', 'Punchana', 'San Juan Bautista'],
+      },
+      { id: '1602', name: 'Alto Amazonas', districts: ['Yurimaguas', 'Balsapuerto', 'Jeberos'] },
+    ],
+  },
+  {
+    id: '17',
+    name: 'Madre de Dios',
+    provinces: [
+      {
+        id: '1701',
+        name: 'Tambopata',
+        districts: ['Tambopata (Puerto Maldonado)', 'Inambari', 'Laberinto'],
+      },
+    ],
+  },
+  {
+    id: '18',
+    name: 'Moquegua',
+    provinces: [
+      {
+        id: '1801',
+        name: 'Mariscal Nieto',
+        districts: ['Moquegua', 'Samegua', 'Torata', 'San Antonio'],
+      },
+      { id: '1803', name: 'Ilo', districts: ['Ilo', 'Pacocha', 'El Algarrobal'] },
+    ],
+  },
+  {
+    id: '19',
+    name: 'Pasco',
+    provinces: [
+      {
+        id: '1901',
+        name: 'Pasco',
+        districts: ['Chaupimarca', 'Yanacancha', 'Simon Bolivar', 'Tinyahuarco'],
+      },
+    ],
+  },
+  {
+    id: '20',
+    name: 'Piura',
+    provinces: [
+      {
+        id: '2001',
+        name: 'Piura',
+        districts: ['Piura', 'Castilla', 'Catacaos', 'Tambogrande', 'Veintiseis de Octubre'],
+      },
+      {
+        id: '2006',
+        name: 'Sullana',
+        districts: ['Sullana', 'Bellavista', 'Marcavelica', 'Querecotillo'],
+      },
+      { id: '2007', name: 'Talara', districts: ['Pariñas', 'Lobitos', 'Mancora', 'Organos'] },
+    ],
+  },
+  {
+    id: '21',
+    name: 'Puno',
+    provinces: [
+      {
+        id: '2101',
+        name: 'Puno',
+        districts: ['Puno', 'Acora', 'Capachica', 'Chucuito', 'Plateria'],
+      },
+      { id: '2111', name: 'San Román', districts: ['Juliaca', 'Cabana', 'San Miguel'] },
+    ],
+  },
+  {
+    id: '22',
+    name: 'San Martín',
+    provinces: [
+      { id: '2201', name: 'Moyobamba', districts: ['Moyobamba', 'Calzada', 'Soritor'] },
+      {
+        id: '2209',
+        name: 'San Martín',
+        districts: ['Tarapoto', 'Morales', 'La Banda de Shilcayo'],
+      },
+    ],
+  },
+  {
+    id: '23',
+    name: 'Tacna',
+    provinces: [
+      {
+        id: '2301',
+        name: 'Tacna',
+        districts: [
+          'Tacna',
+          'Alto de la Alianza',
+          'Ciudad Nueva',
+          'Gregorio Albarracin',
+          'Pocollay',
+        ],
+      },
+    ],
+  },
+  {
+    id: '24',
+    name: 'Tumbes',
+    provinces: [
+      { id: '2401', name: 'Tumbes', districts: ['Tumbes', 'Corrales', 'La Cruz', 'San Jacinto'] },
+    ],
+  },
+  {
+    id: '25',
+    name: 'Ucayali',
+    provinces: [
+      { id: '2501', name: 'Coronel Portillo', districts: ['Pucallpa', 'Manantay', 'Yarinacocha'] },
+    ],
+  },
+];
