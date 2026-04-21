c2f42c73c484a3e7c4295ab6849086bd5e473f5e
Angel Gala <galafloresangelemilio@gmail.com>
2026-04-21 14:25:29 -0500

    removed nextjs project

--- apps/web/components/ContentWidth.tsx
+++
@@ -1,9 +0,0 @@
-import { type ReactNode } from 'react';
-
-interface ContentWidthProps {
-  children: ReactNode;
-}
-
-export const ContentWidth = ({ children }: ContentWidthProps) => {
-  return <div className="mx-auto max-w-[1400px] px-4">{children}</div>;
-};
